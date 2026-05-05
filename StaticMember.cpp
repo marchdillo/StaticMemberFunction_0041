5833d738f29cb791ae2a9a005a9c08c301c2e24a
kevinno.limiy@gmail.com <kevinno.limiy@gmail.com>
2026-05-05 15:59:22 +0700

    membuat object dan memberi nilai

--- StaticMember.cpp
+++ StaticMember.cpp
@@ -1,28 +1,26 @@
 #include <iostream>
 using namespace std;
 
-class Mahasiswa { //membuat class mahasiswa
+class Mahasiswa { 
 
 public:
-    static int nim; //deklarasi variabel static yaitu variabel nim
-    //deklarasi variabel member
+    static int nim; 
     int id;
     string nama;
 
-    //deklarasi prosedur setID() dan printAll()
+   
     void setID();
     void printAll();
 
-    //pembuatan constructor Mahasiswa dengan parameter pnama
+   
     Mahasiswa(string pnama) :nama(pnama) {
         setID();
     }
 };
 
-//memberi value ke variabel nim dari class mahasiswa
+
 int Mahasiswa::nim = 10;
 
-//mengimplementasikan prosedur - prosedur di luar class
 void Mahasiswa::setID() {
     id = ++nim;
 };
@@ -33,5 +31,11 @@ void Mahasiswa::printAll() {
 }
 
 int main() {
- 
+//membuat object dan memberi nilai
+    Mahasiswa mhs1("Lia Kurnia");
+    Mahasiswa mhs2("Asroni");
+    Mahasiswa mhs3("Andi Kurniawan");
+    Mahasiswa mhs4("Joko Purbo");
+
+  
 }
